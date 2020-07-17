b52bd97baf45880065a590345f5f7bd5d606be4b
leanback2020 <post@leanback.no>
2020-07-17 23:23:23 +0200

    Added web-config and server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const path = require("path")
+const PORT = process.env.PORT || 4000
+const app = new express()
+app.use(express.static(path.join(__dirname, "dist")))
+app.get("*", (req, res) => res.sendFile(__dirname + "/dist/index.html"))
+app.listen(PORT, function () {
+  console.log("App listening on port " + PORT)
+})
