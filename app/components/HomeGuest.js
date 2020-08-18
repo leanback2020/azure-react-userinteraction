2b01351f37049c3a6784a1b61062762949ad15c5
leanback2020 <post@leanback.no>
2020-08-18 21:05:42 +0200

    New homepage

--- app/components/HomeGuest.js
+++ app/components/HomeGuest.js
@@ -202,7 +202,7 @@ function HomeGuest() {
     <Page title="Home" wide={true}>
       <div className="row align-items-center">
         <div className="col-lg-7 py-3 py-md-5">
-          <h1 className="display-3">Remember Writing?</h1>
+          <h1 className="display-3">Welcome</h1>
           <p className="lead text-muted">Are you sick of short tweets and impersonal &ldquo;shared&rdquo; posts that are reminiscent of the late 90&rsquo;s email forwards? We believe getting back to actually writing is the key to enjoying the internet again.</p>
         </div>
         <div className="col-lg-5 pl-lg-5 pb-3 py-lg-5">
