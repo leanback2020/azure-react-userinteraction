2327ab175f70a21a9c62842ee585447ba7f04e91
leanback2020 <post@leanback.no>
2020-08-18 18:36:53 +0200

    change

--- server.js
+++ server.js
@@ -6,4 +6,5 @@ app.use(express.static(path.join(__dirname, "dist")))
 app.get("*", (req, res) => res.sendFile(__dirname + "/dist/index.html"))
 app.listen(PORT, function () {
   console.log("App listening on port " + PORT)
+  console.log("...")
 })
